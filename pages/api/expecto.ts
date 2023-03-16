dcc03451d3be48436b4c37d66d4b1417206b9d47
Dan Tech <danhtran.dev@outlook.com>
2023-03-16 12:20:02 +0700

    apply rate limit

--- pages/api/expecto.ts
+++ pages/api/expecto.ts
@@ -1,10 +1,34 @@
 import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
+import {Ratelimit} from "@upstash/ratelimit";
+import {Redis} from "@upstash/redis";
+const requestIp = require('request-ip');
+
+const redis = new Redis({
+    url: 'chief-pipefish-33349.upstash.io',
+    token: '885c36ef32e842a5bfd10c3488cad875',
+  });
+  // Create a new ratelimiter, that allows 5 requests per 5 seconds
+const ratelimit = new Ratelimit({
+    redis: redis,
+    limiter: Ratelimit.fixedWindow(1, "30 s"),
+  });
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    const identifier = requestIp.getClientIp(req);
+    // console.log(identifier);
+    const result = await ratelimit.limit(identifier);
+
+    if (!result.success) {
+        res.status(200).json({ generatedText: "You're spelling too much at a same time. Stay calm my Wiz, Magic loves you <3!" })
+        return
+    }
+    res.setHeader('X-RateLimit-Limit', result.limit)
+    res.setHeader('X-RateLimit-Remaining', result.remaining)
+    
     const { expecto_it } = req.query;
 	console.log(req.query);
 	try {
